0dfc06cb5873c0cec17f204a6f8428fae610dd05
sanjidnet <contact@sanjid.net>
2024-02-21 16:01:24 +1300

    add method to retrieve personal data

--- healthy_sanjid/main.py
+++ healthy_sanjid/main.py
@@ -1,1 +1,20 @@
+# main.py
+import requests
+import json
 
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+def getPersonalInfo():
+    response = requests.get(
+        "https://oura-a8ioop.5sc6y6-4.usa-e2.cloudhub.io/private")
+    return (response.json())
+
+
+if __name__ == "__main__":
+    value = getPersonalInfo()
+    jprint(value)
